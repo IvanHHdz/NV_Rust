d735763ef2156c5874d64f96db2fe6d7eb184b39
IvanHHdz <hernivan336@outlook.com>
2025-09-29 11:21:49 -0600

    ejemplo

--- prueba/src/main.rs
+++ prueba/src/main.rs
@@ -1,38 +1,22 @@
-fn main(){
-    let persona = crear_persona!("Juan", 30, "Calle Falsa 123", "555-1234", "juan@example.com");
-    println!("{:?}", persona);
-
-    let persona = crear_persona!("Pablo", 41);
-    println!("{:?}", persona);
+#[tokio::main] 
+async fn main() {
+    // declaramos una variable `resultado` la cual tendrá el resultado de la función suma_lenta()
+    // esta función es asincrónica, por lo que retornará un future
+    // este lo podemos aplicar `.await` ahora o después (lo aplicaremos después)
+    let resultado = suma_lenta(5, 10);  // como no usamos `.await`, el programa sigue
+    // esperamos un segundo
+    println!("Hola, mundo!");           // imprimirá esto
+    // ahora usaremos el `.await`
+    // por lo que se empezará a ejecutar la función `suma_lenta()`
+    // el main se detiene aquí, esperando que se obtenga el valor de `resultado`
+    println!("El resultado es: {}", resultado.await);
 }
 
-#[derive(Debug)]
-struct Persona {
-    nombre: String,
-    edad: u8,
-    direccion: String,
-    telefono: String,
-    email: String,
-}
-
-#[macro_export]
-macro_rules! crear_persona {
-    ($nombre:expr, $edad:expr, $direccion:expr, $telefono:expr, $email:expr) => {
-        Persona {
-            nombre: $nombre.to_string(),
-            edad: $edad,
-            direccion: $direccion.to_string(),
-            telefono: $telefono.to_string(),
-            email: $email.to_string(),
-        }
-    };
-    ($nombre:expr, $edad:expr) => {
-        Persona {
-            nombre: $nombre.to_string(),
-            edad: $edad,
-            direccion: "N/A".to_string(),
-            telefono: "N/A".to_string(),
-            email: "N/A".to_string(),
-        }
-    };
+// esta es la función 
+async fn suma_lenta(a: i32, b: i32) -> i32 {
+    // imprimimos algo para ver cuándo se empieza a ejecutar
+    println!("Calculando la suma de {} y {}...", a, b);
+    // hacemos esperar 5 seg
+    tokio::time::sleep(std::time::Duration::from_secs(5)).await;
+    a + b   // retornamos el valor
 }
